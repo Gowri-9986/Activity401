2e8dddfc7e7b9cf1352f1f2dd5966199f6a1a7ef
Gowri-9986 <peram.gowri@capgemini.com>
2025-02-18 10:35:01 +0530

    Add files via upload

---
+++ Activity401/src/main/java/com/Activity/Activity401/Activity401Application.java
@@ -0,0 +1,13 @@
+package com.Activity.Activity401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activity401Application.class, args);
+	}
+
+}
---
+++ Activity401/src/test/java/com/Activity/Activity401/Activity401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Activity.Activity401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
